09d946c4e1c7fea18001768f60d4621d6261da9e
Martin Infante <martin.infante@patagoniansys.com>
2022-11-30 08:28:44 -0300

    Add demo material

---
+++ dummy.py
@@ -0,0 +1,10 @@
+def main():
+    """
+    Dummy main function for the Patagonian Flash Tech Talk.
+    """
+    print("Hello Patagonians!")
+    return
+
+
+if __name__ == "__main__":
+    main()
---
+++ test/test_dummy.py
@@ -0,0 +1,16 @@
+import unittest
+
+
+class TestDummy(unittest.TestCase):
+    """
+    This class implements a dummy test for the Patagonian Flash Tech Talk.
+    """
+
+    def test_dummy(self):
+        """
+        This test will always pass.
+        """
+        self.assertTrue(
+            True,
+            msg="This test should always pass"
+        )
